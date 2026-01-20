ec13fc5349362cbe7f5d9a27410fac0ce0041f88
JunHaoOng <114891506+HaoHaoDoesDev@users.noreply.github.com>
2026-01-20 16:41:20 +0800

    feat: added authentication page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,48 +5,7 @@ import { Button } from "@/components/ui/button";
 import userRoutes from "@/config/user-routes";
 import { useRouter } from "next/navigation";
 import CardNav from "@/components/ui/nav-bar";
-
-const items = [
-  {
-    label: "About",
-    bgColor: "#2196F3",
-    textColor: "#fff",
-    links: [
-      { label: "Documentation", ariaLabel: "About Software", href: "/about" },
-      { label: "Team", ariaLabel: "About Team", href: "/careers" },
-    ],
-  },
-  {
-    label: "Features",
-    bgColor: "#2196F3",
-    textColor: "#fff",
-    links: [
-      {
-        label: "Featured",
-        ariaLabel: "Project Features",
-        href: "/projects/featured",
-      },
-    ],
-  },
-  {
-    label: "Contact",
-    bgColor: "#2196F3",
-    textColor: "#fff",
-    links: [
-      {
-        label: "Email",
-        ariaLabel: "Email us",
-        href: "mailto:hello@example.com",
-      },
-      { label: "Twitter", ariaLabel: "Twitter", href: "https://twitter.com" },
-      {
-        label: "LinkedIn",
-        ariaLabel: "LinkedIn",
-        href: "https://linkedin.com",
-      },
-    ],
-  },
-];
+import { items } from "@/data/card-nav-items";
 
 export default function LandingPage() {
   const router = useRouter();
---
+++ frontend/src/data/card-nav-items.tsx
@@ -0,0 +1,41 @@
+export const items = [
+  {
+    label: "About",
+    bgColor: "#2196F3",
+    textColor: "#fff",
+    links: [
+      { label: "Documentation", ariaLabel: "About Software", href: "/about" },
+      { label: "Team", ariaLabel: "About Team", href: "/careers" },
+    ],
+  },
+  {
+    label: "Features",
+    bgColor: "#2196F3",
+    textColor: "#fff",
+    links: [
+      {
+        label: "Featured",
+        ariaLabel: "Project Features",
+        href: "/projects/featured",
+      },
+    ],
+  },
+  {
+    label: "Contact",
+    bgColor: "#2196F3",
+    textColor: "#fff",
+    links: [
+      {
+        label: "Email",
+        ariaLabel: "Email us",
+        href: "mailto:hello@example.com",
+      },
+      { label: "Twitter", ariaLabel: "Twitter", href: "https://twitter.com" },
+      {
+        label: "LinkedIn",
+        ariaLabel: "LinkedIn",
+        href: "https://linkedin.com",
+      },
+    ],
+  },
+];
