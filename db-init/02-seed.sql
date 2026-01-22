86d3c58ffab0cb9f643ec0e2610da2562fdaff9c
lucasyeo69 <lucas.yeo69@gmail.com>
2026-01-22 17:30:26 +0800

    feat: Updated user Id to 9 digits

--- db-init/02-seed.sql
+++ db-init/02-seed.sql
@@ -1,3 +1,11 @@
+CREATE OR REPLACE FUNCTION generate_random_id() RETURNS INTEGER AS $$
+BEGIN
+    RETURN floor(random() * (999999999 - 100000000 + 1) + 100000000)::INTEGER;
+END;
+$$ LANGUAGE plpgsql;
+
+ALTER TABLE users ALTER COLUMN id SET DEFAULT generate_random_id();
+
 INSERT INTO users (email, password_hash, role)
 VALUES (
     'admin@gmail.com', 
