c8dd2ef6a176dc80658d6c74d41e01ca74c7feef
lucasyeo69 <lucas.yeo69@gmail.com>
2026-02-09 15:59:02 +0800

    Fix: Added rate limit to API.

--- services/user-service/src/index.ts
+++ services/user-service/src/index.ts
@@ -1,16 +1,20 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 import { db } from "./config/db.js";
 import fileRoutes from "./routes/fileRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 5002; 
-
+const fileRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100, 
+});
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/files", fileRoutes);
+app.use("/api/files", fileRateLimiter, fileRoutes);
 
 const start = async () => {
   try {
